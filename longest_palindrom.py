975f6967a972af2cd1289b532109dfd6f537e5f8
Your Name <your.email@example.com>
2024-04-05 21:41:46 +0800

    woah did so many qns today. bang gang loj (5/4/24)

--- longest_palindrom.py
+++ longest_palindrom.py
@@ -1,23 +1,27 @@
-
-
+def from_middle(s,left,right):
+        while left >= 0 and right <= len(s) - 1 and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return (s[left+1:right])
+         
 def longestPalindrome(s):
         """
         :type s: str
         :rtype: str
         """
-        #going from the middle of the string s
-        left = len(s) // 2
-        right = left
-        print(f"initial value of right : {right} and left is: {left}")
-        while left >= 0 and right <= len(s) - 1 and s[left] == s[right]:
-            print("entering the loop")
-            left -= 1
-            right += 1
-            
-        print(f"left value is: {left}")
-        print(f"right value is {right}")
+        list = ''
+        for i in range(len(s)):
+            odd_palindrome = from_middle(s,i,i)
+            if len(odd_palindrome) > len(list):
+                list = odd_palindrome
+            even_palindrome = from_middle(s,i,i+1)
+            if len(even_palindrome) > len(list):
+                list = even_palindrome
+            return list
+                
 
-longestPalindrome("abaab")
+value = longestPalindrome("ac")
+print(f"final value is:{value}")
                 
                         
                         
